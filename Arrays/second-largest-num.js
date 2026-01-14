419ff82589b8c4d60fdd22581f014e0525149c5e
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-01-14 19:21:26 +0530

    completed brute force approach for second-largest-number

--- Arrays/second-largest-num.js
+++ Arrays/second-largest-num.js
@@ -1,2 +1,18 @@
+// 1. second largest number
+// Given an array of size N ,print the second largest number from an array
 
+// Input : [12,35,1,10,34,1] ====> output: 34
+// Input : [10,5,10]  ===> output: 5
 
+// Input : [12,35,1,10,34,1] = [1,1,10,12,34,35]
+
+function secondLargest(arr) {
+  const uniqueArr = Array.from(new Set(arr)); // [10,5]
+  uniqueArr.sort((a, b) => b - a); // [10,5]
+  if (uniqueArr.length >= 2) {
+    return uniqueArr[1]; // 5
+  } else {
+    return -1;
+  }
+}
+console.log(secondLargest([10,5,10]));

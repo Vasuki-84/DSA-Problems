f7b734f6048a1ae3c98ab512ddc6637ddc38fff0
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-01-26 11:20:49 +0530

    factorial of a number

---
+++ Recursion/Factorial_of_n.js
@@ -0,0 +1,12 @@
+// Factorial of Number
+
+// Factorial of 5 = 5*4*3*2*1 = 120
+
+function Factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * Factorial(n - 1);
+  }
+}
+console.log(Factorial(3));
